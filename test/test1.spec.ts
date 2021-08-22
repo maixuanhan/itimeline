889e31b0bcbb75deac9fb970e3583cb28e202aaf
Han Mai <cafe2310@gmail.com>
2021-08-22 12:03:30 +0800

    Project initialized

---
+++ test/test1.spec.ts
@@ -0,0 +1,7 @@
+const should = require('chai').should()
+
+describe('Test getIp() function', function () {
+    it("Empty array should have length 0", function () {
+        [].should.have.length(0);
+    });
+});
